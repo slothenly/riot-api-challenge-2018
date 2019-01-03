4530bc657486d5d46f46342ba2c5586824afd634
Beatrice <Beatrice@DESKTOP-FOGA9V5>
2019-01-03 09:57:20 -0800

    Setup is completed, RiotSharp now fully accessible!

--- League-API-Proj/Form1.cs
+++ League-API-Proj/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RiotSharp;
 
 namespace League_API_Proj
 {
@@ -15,6 +16,7 @@ namespace League_API_Proj
         public Form1()
         {
             InitializeComponent();
+            var api = RiotApi.GetInstance("AN-ACTUAL-API-KEY");
         }
     }
 }
